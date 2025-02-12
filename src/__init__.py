3cb9b0051c7db756b342047740154bd35fc6dc63
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2025-02-12 14:20:31 +0800

    add Entity

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,15 @@
+from .DomainEvent import DomainEvent, IDomainEvent
+from .Entity import Entity, IEntity
 from .Error import Error, ErrorType
 from .ValueObject import SingleValueObject, ValueObject
 
-__all__ = ["Error", "ErrorType", "ValueObject", "SingleValueObject"]
+__all__ = [
+    "Entity",
+    "IEntity",
+    "Error",
+    "ErrorType",
+    "ValueObject",
+    "SingleValueObject",
+    "DomainEvent",
+    "IDomainEvent",
+]
